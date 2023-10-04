092ac64019f9891fb83310c6cfc28344009cf055
batMaxCom <batman181194@gmail.com>
2023-10-04 19:14:46 +0300

    Create project, connect db/migration, add models

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+DB_HOST = os.environ.get("POSTGRES_HOST")
+DB_PORT = os.environ.get("POSTGRES_PORT")
+DB_NAME = os.environ.get("POSTGRES_DB_NAME")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASS = os.environ.get("POSTGRES_PASSWORD")
+
