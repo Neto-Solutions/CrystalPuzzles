3f61b25d641867aaad7cfa8e89e5a8b8977a6179
batMaxCom <batman181194@gmail.com>
2023-10-10 14:48:12 +0300

    Add tables v2

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -6,11 +6,12 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from src.database import metadata
 
-from src.auth.models import user, user_type, user_rank, user_group, task
-from src.lesson.models import lesson, check, space, training_check, training
-from src.schedule.models import schedule
+from src.database import Base
+from src.auth.models_v2 import User, UserType, UserGroup, UserRank, Task
+from src.lesson.models_v2 import Lesson, Training, TrainingCheck, Space, Check
+from src.schedule.models_v2 import Schedule
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -35,7 +36,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- backend/src/database.py
+++ backend/src/database.py
@@ -1,14 +1,16 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-Base = declarative_base()
-metadata = Base.metadata
+
+class Base(DeclarativeBase):
+    pass
+
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
