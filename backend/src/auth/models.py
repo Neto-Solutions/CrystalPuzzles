69fc65bd0f8b69747ef1fec01dd9efe51f3b7bf3
batMaxCom <batman181194@gmail.com>
2023-10-16 18:46:03 +0300

    Add routers register, login, logout

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -1,55 +1,61 @@
+import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
 import sqlalchemy as sa
-from src.database import metadata
+from src.database import Base
 
 
-user = sa.Table(
-    "user",
-    metadata,
-    sa.Column("id", sa.BigInteger, primary_key=True),
-    sa.Column("email", sa.String(254), nullable=False),
-    sa.Column("password", sa.String(128), nullable=False),
-    sa.Column("is_superuser", sa.Boolean, default=False, nullable=False),
-    sa.Column("is_active", sa.Boolean, default=True, nullable=False),
-    sa.Column("is_verified", sa.Boolean, default=False, nullable=False),
-    sa.Column("firstname", sa.String(50), nullable=False),
-    sa.Column("lastname", sa.String(50), nullable=False),
-    sa.Column("surname", sa.String(50)),
-    sa.Column("birthday", sa.Date, nullable=False),
-    sa.Column("photo", sa.String),
-    sa.Column("type_id", sa.Integer, sa.ForeignKey("user_type.id")),
-    sa.Column("gender", sa.String, nullable=False),
-    sa.Column("group_id", sa.Integer, sa.ForeignKey("user_group.id")),
-    sa.Column("rank_id", sa.Integer, sa.ForeignKey("user_rank.id")),
-    sa.Column("contact", sa.String)
-)
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[int] = mapped_column(sa.String(254), nullable=False)
+    hashed_password: Mapped[int] = mapped_column(sa.String(128), nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
+    firstname: Mapped[str] = mapped_column(sa.String(50), nullable=True)
+    lastname: Mapped[str] = mapped_column(sa.String(50), nullable=True)
+    surname: Mapped[str] = mapped_column(sa.String(50), nullable=True)
+    birthday = mapped_column(sa.Date, nullable=True)
+    photo: Mapped[str] = mapped_column(nullable=True)
+    type_id: Mapped[int] = mapped_column(sa.ForeignKey("user_type.id"), nullable=True)
+    gender: Mapped[str] = mapped_column(sa.String, nullable=True)
+    group_id: Mapped[int] = mapped_column(sa.ForeignKey("user_group.id"), nullable=True)
+    rank_id: Mapped[int] = mapped_column(sa.ForeignKey("user_rank.id"), nullable=True)
+    contact: Mapped[str] = mapped_column(nullable=True)
 
-user_type = sa.Table(
-    "user_type",
-    metadata,
-    sa.Column("id", sa.BigInteger, primary_key=True),
-    sa.Column('name', sa.String, nullable=False)
-)
 
-user_group = sa.Table(
-    "user_group",
-    metadata,
-    sa.Column("id", sa.BigInteger, primary_key=True),
-    sa.Column("name", sa.String, nullable=False),
-    sa.Column("trainer_id", sa.Integer, sa.ForeignKey("user.id", use_alter=True))
-)
+class UserType(Base):
+    __tablename__ = "user_type"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
 
-user_rank = sa.Table(
-    "user_rank",
-    metadata,
-    sa.Column("id", sa.BigInteger, primary_key=True),
-    sa.Column("name", sa.String, nullable=False),
-    sa.Column("task", sa.Integer, sa.ForeignKey("task.id"))
-)
 
-task = sa.Table(
-    "task",
-    metadata,
-    sa.Column("id", sa.BigInteger, primary_key=True),
-    sa.Column("name", sa.String, nullable=False),
-    sa.Column("description", sa.Text)
-)
+class UserGroup(Base):
+    __tablename__ = "user_group"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    trainer_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("user.id", use_alter=True))
+
+
+class UserRank(Base):
+    __tablename__ = "user_rank"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    task_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("task.id"))
+
+
+class Task(Base):
+    __tablename__ = "task"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    description = mapped_column(sa.Text, nullable=True)
+
+
+class Token(Base):
+    __tablename__ = "token"
+    user_id: Mapped[str] = mapped_column(sa.ForeignKey("user.id"), nullable=False)
+    access_token: Mapped[str] = mapped_column(sa.String(450), primary_key=True)
+    refresh_token: Mapped[str] = mapped_column(sa.String(450), nullable=False)
+    status: Mapped[bool]
+    created_date = mapped_column(sa.DateTime, default=datetime.datetime.now)
---
+++ backend/src/auth/router.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from src.auth.models import User, Token
+from src.auth.schemas import UserCreateSchema, UserReadSchema
+from src.auth.utils import get_user_by_email, hash_password, create_access_token, \
+    get_current_user, authenticate_user, create_refresh_token, oauth2_scheme, jwtdecode
+from src.database import get_async_session
+
+
+router = APIRouter(
+    prefix="/user",
+    tags=["Auth"]
+)
+
+
+@router.get("/account", response_model=UserReadSchema)
+async def account(current_user: User = Depends(get_current_user)):
+    """ Возвращает данные пользователя """
+    return current_user
+
+
+@router.post("/register/", response_model=UserReadSchema)
+async def register(data: UserCreateSchema, session: AsyncSession = Depends(get_async_session)):
+    """ Регистрация пользователя """
+    existing_user = await get_user_by_email(data.email)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="This Email already exists")
+    user_dict = data.model_dump()
+    password = user_dict.pop("password")
+    user_dict["hashed_password"] = hash_password(password)
+    create_user = User(**user_dict)
+    session.add(create_user)
+    await session.commit()
+    return create_user
+
+
+@router.post("/login/")
+async def login(data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_async_session)):
+    """ Войти в учетную запись """
+    user = await authenticate_user(data.username, data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user.email})
+    refresh_token = create_refresh_token(data={"sub": user.email})
+    token_db = Token(user_id=user.id, access_token=access_token, refresh_token=refresh_token, status=True)
+    session.add(token_db)
+    await session.commit()
+    return {"access_token": access_token, "refresh_token": refresh_token}
+
+
+@router.post("/logout/")
+async def logout(
+        token: str = Depends(oauth2_scheme),
+        current_user: User = Depends(get_current_user),
+        session: AsyncSession = Depends(get_async_session)
+):
+    """ Выйти из учетеной записи """
+    query = await session.execute(select(Token).where(
+        Token.user_id == current_user.id,
+        Token.access_token == token
+    ))
+    existing_token = query.scalar()
+    info = []
+    if (datetime.utcnow() - existing_token.created_date).days > 1:
+        info.append(existing_token.user_id)
+    if info:
+        existing_token.delete()
+        await session.commit()
+        return {"message": "The access token has expired"}
+    existing_token.status = False
+    session.add(existing_token)
+    await session.commit()
+    return {"message": "Logout Successfully"}
+
+
+@router.post('/change-password')
+async def change_password():
+    pass
