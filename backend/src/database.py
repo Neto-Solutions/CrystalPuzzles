83bb0fd728fbc5858c39add1f630ebc3adff0e7c
batMaxCom <batman181194@gmail.com>
2023-12-13 20:24:34 +0300

    Add UserType initializer црwhen run app

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,21 +1,38 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 class Base(DeclarativeBase):
     pass
 
 
-engine = create_async_engine(DATABASE_URL)
+""" Async session"""
+DATABASE_URL_ASYNC = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL_ASYNC)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+""" Sync session"""
+DATABASE_URL_SYNC = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_engine(DATABASE_URL_SYNC)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session)
+
+
+def get_sync_session() -> Generator[Session, None, None]:
+    try:
+        with SessionLocal() as db:
+            yield db
+    finally:
+        db.close()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
+from sqlalchemy import func, select
 
+from src.auth.models import UserType
 from src.auth.router import router as auth_routers
+from src.database import async_session_maker
 
 app = FastAPI()
+
+
+async def init_user_type():
+    async with async_session_maker() as session:
+        user_type_count_query = await session.execute(select(func.count('*')).select_from(UserType))
+        if user_type_count_query.scalar() == 0:
+            session.add(UserType(name="admin"))
+            session.add(UserType(name="trainer"))
+            session.add(UserType(name="student"))
+            await session.commit()
+
+
+# Добавление инициализации данных в событие startup
+@app.on_event("startup")
+async def startup_event():
+    await init_user_type()
 
 
 @app.get("/")
