000a1eadfb5f705cdf84999f10141b9f944c594c
dinar <saetonskie@mail.ru>
2024-02-21 19:45:40 +0300

    part of login_page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import React from 'react';
+import Login_page from './pages/Login_page_dir/Login_page';
 
-function App() {
-  return (
-    <div className="App">
-      <p>CRYSTALPUZZLES</p>
-    </div>
-  );
+
+
+
+function App()  {
+    return (
+      <div className="App">
+        <Login_page/>
+      </div>
+    )
 }
+
 
 export default App;
