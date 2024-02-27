a53e1881dde2b8f0ead2a2b8d511284d0be35895
dinar <saetonskie@mail.ru>
2024-02-27 18:40:02 +0300

    completed check in pages

--- frontend/src/pages/Login_page_dir/components/Login_main.js
+++ frontend/src/pages/Login_page_dir/components/Login_main.js
@@ -1,42 +1,43 @@
 import React from "react"
-import '../components_css/Login_main.css';
+
  
 function Login_main() {
       return (
-        <div className="main_container">
-            <h1 className="main_container_header">
+        <div className="login_main_container">
+            <h1 className="login_main_container_header">
                 Войти
             </h1>
             <form className="login_form">
-                <label className="choose_your_speciality">
-                    <p className="choose_your_speciality_description">Выберите вашу специальность</p>
-                    <select className="speciality_select"></select>
+                <label className="login_choose_your_speciality">
+                    <p className="login_choose_your_speciality_description">Выберите вашу специальность</p>
+                    <select className="login_speciality_select"></select>
                 </label>
-                <label className="email_input_cont">
-                    <p className="email_input_description">Ваш e-mail</p>
-                    <input className="email_input" type="text"/>
+                <label className="login_email_input_cont">
+                    <p className="login_email_input_description">Ваш e-mail</p>
+                    <input className="login_email_input" type="text"/>
                 </label >
-                <label className="password_input_cont">
-                    <p className="password_input_cont_description">Пароль</p>
-                    <input className="password_input" type="password"/>
+                <label className="login_password_input_cont">
+                    <p className="login_password_input_cont_description">Пароль</p>
+                    <input className="login_password_input" type="password"/>
+                    <a href="#" class="login_password_control"></a>
                 </label>
-                <div className="froget_pass_cont">
-                    <a href="#" className="forget_pass">Забыли пароль?</a>
+                <div className="login_froget_pass_cont">
+                    <a href="#" className="login_forget_pass">Забыли пароль?</a>
                 </div>
                 
-                <label className="confidence_politic_cont">
-                    <input className="confidence_politic_checkbox" type="checkbox"/>
-                    <p className="confidence_politic_cont_descr">Подтверждая, вы соглашаетесь на обработку персональных 
-                    данных и c <a className="confidence_politic_link" href="#">политикой конфиденциальности</a></p>
+                <label className="login_confidence_politic_cont">
+                    <input className="login_confidence_politic_checkbox" type="checkbox"/>
+                    <p className="login_confidence_politic_cont_descr">Подтверждая, вы соглашаетесь на обработку персональных 
+                    данных и c <a className="login_confidence_politic_link" href="#">политикой конфиденциальности</a></p>
                 </label>
-                <div>
-                    <button>Войти</button>
+                <div className="login_enter_btn_cont">
+                    <button className="login_enter_btn">Войти</button>
                 </div>
-                <div>
-                    <a href="#">Нет аккаунта?</a>
+                <div className="login_acc_registr_link_cont">
+                    <a href="#" className="login_acc_registr_link">Нет аккаунта?</a>
                 </div>
-                <div>
-                    <button>Зарегистрироваться</button>
+                <div className="login_acc_registr_btn_cont">
+                    <button className="login_acc_registr_btn">Зарегистрироваться</button>
                 </div>
                 
             </form>
