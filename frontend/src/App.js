b61cd820e37915399222fba8d33a063761620f10
dinar <saetonskie@mail.ru>
2024-03-03 22:05:46 +0300

    completed oop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import React from 'react';
-import Registration_page from './pages/Registration_page_dir/Registration_page';
-import Login_page from './pages/Login_page_dir/Login_page';
 import './css/checkin_forms/Checkin_forms.css';
 import './css/checkin_forms/Checkin_header.css';
+import './css/general_page_methodist/General_methodist_page.css';
+import Registration_page from './pages/Registration_page_dir/Registration_page';
+import Login_page from './pages/Login_page_dir/Login_page';
 import Confident_page from './pages/Confident_politic_page_dir/Confident_page'
 import Registration_student_page from './pages/Registration_student_page_dir/Registration_student_page'
- 
+import General_methodist_page from './pages/General_page_desctop_dir/General_methodist_page'
 
 function App()  {
     return (
       <div className="App">
-        <Registration_page/>
-        <Registration_student_page/>
-        <Login_page/>
-        <Confident_page/>
+        <General_methodist_page/>
       </div>
     )
 }
