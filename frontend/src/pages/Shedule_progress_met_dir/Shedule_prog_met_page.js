01df5fa60b9968492c67e0a67b453e22cb8d5a1b
dinar <saetonskie@mail.ru>
2024-03-14 16:29:05 +0300

    general menu methodist

---
+++ frontend/src/pages/Shedule_progress_met_dir/Shedule_prog_met_page.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Checkin_header from "./components/Checkin_header"
+import Shedule_prog_met from "./components/Shedule_prog_met"
+
+
+function Shedule_prog_met_page() {
+    return(
+        <div>
+            <Checkin_header/>
+            <Shedule_prog_met/>
+        </div>
+    )
+}
+
+export default Shedule_prog_met_page
---
+++ frontend/src/pages/Shedule_progress_met_dir/components/Shedule_prog_met.js
@@ -0,0 +1,42 @@
+import React from "react"
+import General_acc_panel from "./General_acc_panel"
+import General_header from "./General_header"
+
+function Shedule_prog_met() {
+      return (
+        <div className="analytic_met_main_cont">
+            <General_acc_panel/>
+            <div className="analytic_met_header_base_cont">
+                <General_header/>
+                <main className="analytic_met_base_cont">
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div>
+                  </div>
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div>
+                  </div>
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div>
+                  </div>
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div> 
+                  </div>
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div>
+                  </div>
+                  <div className="analytic_met_part">
+                    <div className="analytic_met_photo"></div>
+                    <div className="analytic_met_descr">Дмитриева Анастасия Алексеевна</div>
+                  </div>
+                </main>
+            </div>  
+        </div>
+      )
+}
+
+export default Shedule_prog_met
