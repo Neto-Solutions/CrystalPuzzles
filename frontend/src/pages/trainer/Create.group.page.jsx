88522848a03583b5d85d0728bc65739ef56de0ff
dinar <saetonskie@mail.ru>
2024-04-20 09:31:08 +0300

    completed trainer checklist

--- frontend/src/pages/trainer/Create.group.page.jsx
+++ frontend/src/pages/trainer/Create.group.page.jsx
@@ -1,3 +1,13 @@
+import './Check.list.page.scss';
+import PageContainer from '@components/page.container/Page.container';
+import Button from '@components/button/Button';
+import ChooseButton from '@components/button/Choose.button';
+
 export default function CreateGroupPage() {
-	return <div></div>;
+	return <>
+		<PageContainer.Header title="" />
+		<PageContainer.Body>
+			
+		</PageContainer.Body>
+	</>;
 }
