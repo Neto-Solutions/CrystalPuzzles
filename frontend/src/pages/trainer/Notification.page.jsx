c52f8c65d4be01fa0bd1ecbee53fa0e0f97d3b4b
dinar <saetonskie@mail.ru>
2024-04-20 09:47:09 +0300

    completed train main & notif pages

--- frontend/src/pages/trainer/Notification.page.jsx
+++ frontend/src/pages/trainer/Notification.page.jsx
@@ -1,3 +1,66 @@
+import './Notification.page.scss';
+import PageContainer from '@components/page.container/Page.container';
+import Button from '@components/button/Button';
+import ChooseButton from '@components/button/Choose.button';
+
+
 export default function NotificationPage() {
-	return <div></div>;
+	return (
+	<>
+	<PageContainer.Header title="Главная страница" />
+	<PageContainer.Body>
+	<div className="notifications_trainer_main_cont">
+
+<div className="notifications_trainer_header_base_cont">
+
+	<main className="notifications_trainer_base_cont">
+	  <div className="notifications_trainer_cont">
+		<h2 className="notifications_trainer_header">
+		  Уведомления
+		</h2>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		<div className="notifications_trainer_item">
+		  <div className="notifications_trainer_description">
+			Михаил выполнил все задания
+		  </div>
+		  <a className="notifications_trainer_open_link">Открыть </a>
+		</div>
+		
+	  </div>
+	</main>
+</div>  
+</div>
+	</PageContainer.Body>
+		
+	</>
+);
 }
