4203a8bf6188ddd7e395ef244aebb19bb129389f
Edgard <eins.yeager@gmail.com>
2024-05-03 20:11:53 +0500

    added service worker from pwa template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
@@ -22,3 +23,8 @@ root.render(
 		</Provider>
 	</React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
