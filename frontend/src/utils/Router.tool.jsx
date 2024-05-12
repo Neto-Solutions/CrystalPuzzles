ab97f242d132ca64d68ab12fbbe57bbacd666279
Edgard <eins.yeager@gmail.com>
2024-05-12 12:17:42 +0500

    router tool updates

--- frontend/src/utils/Router.tool.jsx
+++ frontend/src/utils/Router.tool.jsx
@@ -5,16 +5,13 @@ import trainerRoutes from '../routes/trainer.router';
 import kidsRoutes from '../routes/kids.router';
 import { useLayoutEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function RouterTool() {
 	const [show, setShow] = useState(true);
 	const [routes, setRoutes] = useState([]);
 	const navigate = useNavigate();
 	const role = useSelector((state) => state.user.role);
-	const dispatch = useDispatch();
-
-	const roles = ['methodist', 'trainer', 'kids'];
 
 	useLayoutEffect(() => {
 		setRoutes(() => {
@@ -29,25 +26,6 @@ export default function RouterTool() {
 		<div className="router_tool_container">
 			<div className="router_tool_btn" onClick={() => setShow(!show)}></div>
 			<div className={show ? 'router_tool' : 'router_tool router_tool_hide'}>
-				<div className="router_tool_role_container">
-					{roles.map((el, index) => (
-						<>
-							<div
-								key={index}
-								className={
-									el === role
-										? 'router_tool_role router_tool_role_active'
-										: 'router_tool_role'
-								}
-								onClick={() =>
-									dispatch({ type: 'SET_ROLE', payload: el }, navigate('/'))
-								}
-							>
-								{el}
-							</div>
-						</>
-					))}
-				</div>
 				<div className="router_tool_routes_container">
 					{routes.map((el, index) => (
 						<>
