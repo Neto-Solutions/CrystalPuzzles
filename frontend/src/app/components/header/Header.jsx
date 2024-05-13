b4743db56603d39dad73ad8422909399fabbea03
Edgard <eins.yeager@gmail.com>
2024-05-13 16:16:59 +0500

    header update

--- frontend/src/app/components/header/Header.jsx
+++ frontend/src/app/components/header/Header.jsx
@@ -1,10 +1,13 @@
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '@assets/logo.svg';
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
-			<Link to="/"></Link>
+			<Link to="/" className={styles.logo}>
+				<Logo />
+			</Link>
 		</header>
 	);
 }
