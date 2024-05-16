f9735f8260da7dda688b0a0c6ddbae3f7a6568cd
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-05-16 13:00:32 +0100

    Update install_docker_docker-compose.sh

--- devops/install_docker_docker-compose.sh
+++ devops/install_docker_docker-compose.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # uninstall all conflicting packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remo$
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
+    sudo apt-get remove $pkg
+done
 
-
-# Set up Docker's apt repository Add Docker's official GPG key:
+# Set up Docker's apt repository
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates
-curl sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get install -y ca-certificates
+sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
