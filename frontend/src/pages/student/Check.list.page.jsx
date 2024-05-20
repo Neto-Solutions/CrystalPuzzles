e41317defdbde3a012157c21e83da1a2a3824208
Edgard <eins.yeager@gmail.com>
2024-05-20 20:48:03 +0500

    small fix cards

--- frontend/src/pages/student/Check.list.page.jsx
+++ frontend/src/pages/student/Check.list.page.jsx
@@ -1,13 +1,13 @@
 import styles from './Check.list.page.module.scss';
 import Page from '@shared/ui/page/Page';
-import { Card } from '@shared/card';
+import { CardLink } from '@shared/card';
 import { CheckList } from '@features/check.list';
 
 export default function CheckListPage() {
 	return (
 		<Page title="Чек-листы">
 			<CheckList />
-			<Card
+			<CardLink
 				title="Получи награду за выполнение уровня"
 				className={styles.rewart_trio}
 			/>
