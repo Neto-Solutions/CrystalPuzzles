960fc437d77d3c5d8f82306babc450046ee48818
Edgard <eins.yeager@gmail.com>
2024-05-27 12:50:44 +0500

    reward popup, sidebar link to change avatar

--- frontend/src/entities/user/ui/Account.jsx
+++ frontend/src/entities/user/ui/Account.jsx
@@ -3,7 +3,6 @@ import avatar from '@shared/assets/img/avatar.jpg';
 import roleAdaptor from '../helpers/role.adaptor';
 import { useEffect, useMemo, useState } from 'react';
 import { getProfileAvatar } from '../api/profile';
-import { Link } from 'react-router-dom';
 
 export const Account = ({ user }) => {
 	const [userPhoto, setUserPhoto] = useState(
@@ -27,17 +26,16 @@ export const Account = ({ user }) => {
 
 	return (
 		<div className={styles.accaunt_wrap}>
-			<Link to="/avatar">
-				<img
-					src={
-						userPhoto !== 'null' && userPhoto !== null
-							? 'data:image/png;base64,' + userPhoto
-							: avatar
-					}
-					className={styles.avatar}
-					alt=""
-				/>
-			</Link>
+			<img
+				src={
+					userPhoto !== 'null' && userPhoto !== null
+						? 'data:image/png;base64,' + userPhoto
+						: avatar
+				}
+				className={styles.avatar}
+				alt=""
+			/>
+
 			<div>
 				<p className={styles.profession}>{position}</p>
 				<p className={styles.name}>{user.firstname}</p>
---
+++ frontend/src/shared/ui/popups/index.js
@@ -0,0 +1,1 @@
+export { default as RewardsPopup } from './rewards/Rewards';
