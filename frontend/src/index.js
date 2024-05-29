8faf587fa5a172864db569f4fba6e37cdff5e756
Edgard <eins.yeager@gmail.com>
2024-05-29 21:29:44 +0500

    move service worker to public

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,10 @@
 import '@app/styles/index.scss';
 import React, { useMemo } from 'react';
 import ReactDOM from 'react-dom/client';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-
 import { Provider, useSelector } from 'react-redux';
 import { store } from '@app/providers/store';
-
 import { RouterProvider as Router } from 'react-router-dom';
 import { createRouter } from '@app/providers/router';
-
 import { selectUser } from '@entities/user';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -26,8 +21,3 @@ function RouterProvider() {
 	}, [role]);
 	return <Router router={router} />;
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
