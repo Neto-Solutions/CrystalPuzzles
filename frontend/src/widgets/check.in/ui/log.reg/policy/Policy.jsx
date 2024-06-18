df8a2529a538bb519446567eee6ff39e616454cb
Edgard <eins.yeager@gmail.com>
2024-06-18 18:15:03 +0500

    confidantiality redirect

--- frontend/src/widgets/check.in/ui/log.reg/policy/Policy.jsx
+++ frontend/src/widgets/check.in/ui/log.reg/policy/Policy.jsx
@@ -1,5 +1,4 @@
 import styles from './Policy.module.scss';
-import { Link } from 'react-router-dom';
 
 export default function Policy() {
 	return (
@@ -13,10 +12,15 @@ export default function Policy() {
 			/>
 			<label className={styles.label} htmlFor="checkbox">
 				Подтверждая, вы соглашаетесь на обработку персональных данных и c
-				<Link to="/politics" className={styles.politic_link}>
+				<a
+					href="https://drive.google.com/file/d/1oU3TamtASCI3z2CN6RNhO2sCJye0mk8N/view?usp=drive_link"
+					target="_blank"
+					className={styles.politic_link}
+					rel="noopener noreferrer"
+				>
 					{' '}
 					политикой конфиденциальности
-				</Link>
+				</a>
 			</label>
 		</div>
 	);
