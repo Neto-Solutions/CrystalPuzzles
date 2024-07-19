797dcf02f2fe8a2bde6d9e52f7f20683246552f2
Edgard <eins.yeager@gmail.com>
2024-07-19 13:24:54 +0500

    student api added

--- frontend/src/pages/shared/ProfileList/search/Search.jsx
+++ frontend/src/pages/shared/ProfileList/search/Search.jsx
@@ -1,15 +1,23 @@
 import styles from './Search.module.scss';
 import { useEffect, useRef } from 'react';
 import { debounceTime, distinctUntilChanged, fromEvent } from 'rxjs';
+import { getStudents } from '@entities/student';
+import { useNavigate } from 'react-router-dom';
 
 export default function Search() {
 	const inputRef = useRef();
+	const navigate = useNavigate();
 
-	function search() {}
+	async function search() {
+		if (!inputRef.current.value) return;
+		await getStudents(inputRef.current.value)
+			// .then(console.log)
+			.catch(() => navigate('/login'));
+	}
 
 	useEffect(() => {
 		const subscription = fromEvent(inputRef.current, 'keyup')
-			.pipe(debounceTime(1000), distinctUntilChanged())
+			.pipe(debounceTime(500), distinctUntilChanged())
 			.subscribe(() => search());
 		return () => {
 			subscription.unsubscribe();
