7a02586c52e0079b68e9d26331d585d2bd3d5b31
batMaxCom <batmaxcom000@gmail.com>
2024-09-02 08:56:59 +0300

    [Users] Add role in descriptions

--- backend/service/users/routers/trainers_router.py
+++ backend/service/users/routers/trainers_router.py
@@ -25,5 +25,6 @@ async def get_student_list(
         current_user: SupervisorAdminDep,
         filters: UserFilterDep,
 ):
+    """ supervisor, admin """
     trainer_list = await user_service.get_all_by_filter(uow, filters, role="trainer")
     return trainer_list
