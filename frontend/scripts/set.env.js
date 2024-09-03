2d2fff50503a694a71b379c451a791945a71f8a1
einsy <eins.yeager@gmail.com>
2024-09-03 12:54:28 +0500

    added console log for env script

--- frontend/scripts/set.env.js
+++ frontend/scripts/set.env.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const ejs = require('ejs');
 const fs = require('fs');
@@ -5,11 +6,18 @@ const { exit } = require('process');
 
 fs.existsSync('./build/index.html') || exit('build/index.html does not exist');
 
+console.log('reading build/index.html');
 let originHtml = fs.readFileSync('./build/index.html', 'utf8');
 
+console.log('rendering new build/index.html');
 let newHtml = ejs.render(originHtml, {
 	REACT_APP_SERVER_API: process.env.REACT_APP_SERVER_API,
 	REACT_APP_TAG: process.env.REACT_APP_TAG
 });
 
+console.log(
+	'writing build/index.html',
+	process.env.REACT_APP_TAG,
+	process.env.REACT_APP_SERVER_API
+);
 fs.writeFileSync('./build/index.html', newHtml);
