e7b6157f3271744959fde920a90733b467f54900
Olga Its <secirija33@gmail.com>
2024-10-01 23:16:24 +0200

    fix Button component, adaptive ProfilePage

--- frontend/src/pages/shared/Profile/Profile.tsx
+++ frontend/src/pages/shared/Profile/Profile.tsx
@@ -1,11 +1,11 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Page, Button } from '@shared/ui';
-import avatar from '@shared/assets/avatar/0.png';
-import styles from './Profile.module.scss';
-import ProfileInfo from './ProfileInfo/ProfileInfo';
 import joinName from 'entities/profile/assets/joinName';
-import { useState } from 'react';
+import avatar from '@shared/assets/avatar/0.png';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 import EditProfile from './editProfile/EditProfile';
+import styles from './Profile.module.scss';
 
 export default function ProfilePage({ title }: any) {
 	const [edit, setEdit] = useState(false);
@@ -17,14 +17,15 @@ export default function ProfilePage({ title }: any) {
 		<Page title={title}>
 			<EditProfile active={edit} setActive={setEdit} />
 			<div className={styles.container}>
-				<section className={styles.student}>
+				<div className={styles.student}>
 					<img className={styles.avatar} src={user?.photo || avatar} />
-					<Button
-						title="Редактировать данные"
-						className={styles.btn_edit}
-						onClick={() => setEdit(!edit)}
-					/>
-				</section>
+				</div>
+				<Button
+					bgColor="dark"
+					title="Редактировать данные"
+					className={styles.btn_edit}
+					onClick={() => setEdit(!edit)}
+				/>
 				<div className={styles.name}>{joinName(user)}</div>
 				<ProfileInfo user={user} className={styles.info} />
 			</div>
