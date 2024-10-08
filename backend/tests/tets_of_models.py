bbd1e897ed1578e3d0dbedc81593b7df6260f7b4
sergeMMikh <smm@DEM-PC1048.ua.pt>
2024-10-08 15:04:59 +0100

    added test_user_short_schema

---
+++ backend/tests/tets_of_models.py
@@ -0,0 +1,17 @@
+import pytest
+# from backend.common.schema.base_user_schema import UserShortSchema
+from common.schema.base_user_schema import UserShortSchema
+
+def test_user_short_schema():
+    test_user = UserShortSchema(id=1, 
+                         firstname="anytest_firstname", 
+                         lastname="anytest_lastname", 
+                         surname="anytest_surname", 
+                         photo="any_photo")
+    
+    """ Краткая информация о пользователе """
+    assert test_user.id == 1
+    assert test_user.firstname == "anytest_firstname"
+    assert test_user.lastname == "anytest_lastname"
+    assert test_user.surname == "anytest_surname"
+    assert test_user.photo == "any_photo"
