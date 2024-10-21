b388166a39e2d5038f4753ab4fe52364796b7197
einsy <eins.yeager@gmail.com>
2024-10-21 13:42:08 +0500

    added links to footer

--- frontend/src/widgets/footer/Footer.tsx
+++ frontend/src/widgets/footer/Footer.tsx
@@ -1,20 +1,29 @@
 import styles from './Footer.module.scss';
+import { ReactComponent as Logo } from '@shared/assets/header/logo.svg';
 import { ReactComponent as Vk } from './assets/vk.svg';
 import { ReactComponent as Tg } from './assets/tg.svg';
-import neto from './assets/neto.png';
 import { useResize } from '@shared/hooks';
+
 export default function Footer() {
 	const sm = useResize('sm');
 	return (
 		<footer className={styles.footer}>
-			<a href="/#">
-				<img src={neto} alt="Netology" className={styles.netology} />
+			<a href="https://crystalpuzzles.ru/" target="_blank" rel="noreferrer">
+				<Logo height={sm ? 38 : 48} />
 			</a>
 			<div className={styles.socials}>
-				<a href="/#">
+				<a
+					href="https://t.me/crystallpuzzless"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<Tg height={sm ? 38 : 48} />
 				</a>
-				<a href="/#">
+				<a
+					href="https://vk.com/crystalpuzzlesfs"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<Vk height={sm ? 38 : 48} />
 				</a>
 			</div>
